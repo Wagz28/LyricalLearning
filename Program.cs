3b722dd91ab677b6edbec9983cbe41cdb3d5b130
BWagstaff314 <145370921+BWagstaff314@users.noreply.github.com>
2025-04-03 14:06:52 +0100

    Switch over to SQL connection

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-using Azure.Identity;
 using Microsoft.Data.SqlClient;
-using Azure.Core.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
-AzureEventSourceListener.CreateConsoleLogger(); // Debugging logs
 
-var credential = new DefaultAzureCredential();
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
@@ -14,10 +10,6 @@ var app = builder.Build();
 try
 {
     using var connection = new SqlConnection(connectionString);
-    connection.AccessToken = credential.GetToken(
-        new Azure.Core.TokenRequestContext(["https://database.windows.net/.default"])
-    ).Token;
-
     connection.Open();
     Console.WriteLine("Database connection successful!");
 }
@@ -31,5 +23,3 @@ app.UseRouting();
 app.UseAuthorization();
 app.MapRazorPages();
 app.Run();
-
-
